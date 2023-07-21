7e1260845ff06a9aa9c433c2c34a31f78dd9fbd6
treriwen <paul21fe@gmail.com>
2023-07-21 19:59:54 +0200

    ADD :  map dungeon
    add multiple map and tileset

---
+++ map/salle/dungeon/dungeon2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="dungeon2" tilewidth="16" tileheight="16" tilecount="1024" columns="32">
+ <image source="../../../2D Pixel Dungeon Asset Pack/Dungeon_Tileset_at.png" trans="ffffff" width="512" height="512"/>
+</tileset>
