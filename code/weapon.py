bacef2d42cd32430fbbf89baddfd7c1fa10aa5da
Treriwen <paul21fe@gmail.com>
2023-07-27 13:56:53 +0200

    DEBUG : weapon.update
    self.image work and stop crash, no need to use a full png of sprite

--- code/weapon.py
+++ code/weapon.py
@@ -9,7 +9,7 @@ class Weapon(pygame.sprite.Sprite):
         super().__init__()
         self.player = player
         self.sprite_sheet = pygame.image.load(sprite)
-        self.image = self.get_image(0, 0)
+        self.image = self.get_image(0, 80)
         self.image.set_colorkey([0, 0, 0])
         self.rect = self.image.get_rect()
         self.angle = 0
@@ -26,14 +26,14 @@ class Weapon(pygame.sprite.Sprite):
         mouse_position_X, mouse_position_Y = pygame.mouse.get_pos()
 
         # Calculer l'angle entre le joueur et la position de la souris
-        dx = mouse_position_X
-        dy = mouse_position_Y
+        dx = mouse_position_X - self.rect.centerx
+        dy = mouse_position_Y - self.rect.centery
         self.angle = math.degrees(math.atan2(-dy, dx))
 
-        self.image = pygame.transform.rotate(self.sprite_sheet, self.angle)
-        self.rect = self.image.get_rect(center=self.rect.center)
+        self.image_weapon = pygame.transform.rotate(self.image, self.angle)
+        self.rect = self.image_weapon.get_rect(center=self.rect.center)
 
     def get_image(self, x, y):
-        image = pygame.Surface([32, 32])
-        image.blit(self.sprite_sheet, (0, 0), (x, y, 32, 32))
+        image = pygame.Surface([32, 16])
+        image.blit(self.sprite_sheet, (0, 0), (x, y, 32, 16))
         return image
