2df799b2ab0b733c80345c3c1c89c43d1c510b9a
treriwen <paul21fe@gmail.com>
2023-07-28 23:49:07 +0200

    MODIF : weapon mouvement

--- code/weapon.py
+++ code/weapon.py
@@ -27,17 +27,18 @@ class Weapon(pygame.sprite.Sprite):
 
         # Obtenir les coordonnées locales de la souris par rapport au joueur
         mouse_x, mouse_y = pygame.mouse.get_pos()
-        local_mouse_position = mouse_x - self.player.rect.x, mouse_y - self.player.rect.y
+        mouse_x = mouse_x-400
+        mouse_y = mouse_y - 300
 
         # Calculer l'angle en radians entre le joueur et la position de la souris
-        angle_rad = math.atan2(local_mouse_position[1], local_mouse_position[0])
+        angle_rad = math.atan2(mouse_x, mouse_y)
 
         # Convertir l'angle en degrés
         angle_deg = math.degrees(angle_rad)
-        print(angle_deg)
+        angle_deg = angle_deg-90
 
         # Effectuer la rotation autour du centre de l'image
-        self.image = pygame.transform.rotate(self.original_image, -angle_deg)
+        self.image = pygame.transform.rotate(self.original_image, angle_deg)
         self.rect = self.image.get_rect(center=self.rect.center)
 
     def get_image(self, x, y):
